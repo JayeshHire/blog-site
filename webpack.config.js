02c4c8dd7082ead8deea8a648513dc42a385cdee
jayeshhire <jayeshhire96@gmail.com>
2025-10-02 21:08:04 +0530

    made some changes in the webpack config and js files for proper bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js', // Your main client-side JavaScript file
+  // mode: "development",
+  entry: './main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'), // Output directory for the bundled file
-  },
+  }
   // Add loaders and plugins as needed for your project (e.g., Babel for ES6+)
 };
