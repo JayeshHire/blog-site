d93751a5c3b8f93c11ca50899bc8d9ab43c5b52a
jayeshhire <jayeshhire96@gmail.com>
2025-10-17 13:34:26 +0530

    Now the TableTool model is inheriting from ToolDataModel class

--- model/table_tool_model.py
+++ model/table_tool_model.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 from tool_model import ToolDataModel
 
 
-class TableTool(SQLModel, table=True):
+class TableTool(ToolDataModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     content: Dict[str, Any] = Field(sa_column=Column(JSON))
 
