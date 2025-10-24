1861be4e87673e9b1cd8863c648a19222db9a1bd
jayeshhire <jayeshhire96@gmail.com>
2025-10-24 09:32:23 +0530

    update_ToolMD func was fed the wrong tool_name. It was updating the tool_md for the tool with the older block type
    update_ToolMD func was provided with the wrong tool_name. Hence, it was
    making the same changes as they existed for the tool_name. This error
    has been resolved in this commit

--- dependencies/editorjs_data_store.py
+++ dependencies/editorjs_data_store.py
@@ -14,6 +14,10 @@ def add_tool_md(session: Session,
                 tool_id: uuid.UUID ) -> Tuple[
                     Session, tool_model.ToolMD
                 ]:
+    tool_id = session.exec(
+        select(tool_model.Tool)
+        .where(tool_model.Tool.name == block.type)
+    ).one().id 
     tool_md = tool_model.ToolMD(
         sequence= block.sequence,
         article_id= article_id,
@@ -177,7 +181,7 @@ def update_ToolMD(session: Session,
         select(tool_model.Tool)
         .where(tool_model.Tool.name == tool_name)
     ).one().id
-    tool_md.id = new_tool_id
+    tool_md.tool_id = new_tool_id
     tool_md.block_id = block_id
     session.add(tool_md)
     return (session, tool_md)
@@ -233,8 +237,10 @@ def update_code_tool_data(session: Session,
         # session.add(ct_tbl)
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
+        
+        # raise ValueError(f"{tool_md}")
         session, ct_tbl = add_code_tool_data(session, 
                            block=block, 
                            tool_md_id= tool_md.id
@@ -282,7 +288,7 @@ def update_header_tool_data(session: Session,
         # session.add(header_tool_inst)
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
         session, header_tool_inst = add_header_tool_data(
             session=session,
@@ -344,7 +350,7 @@ def update_list_tool_data(session: Session,
 
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
         session, lt_tbl = add_list_tool_data(session=session,
                            block=block,
@@ -383,7 +389,7 @@ def update_paragraph_tool_data(session: Session,
 
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
         session, p_tbl = add_paragraph_tool_data(session, block, tool_md.id)
     return (session, p_tbl)
@@ -421,7 +427,7 @@ def update_quote_tool_data(session: Session,
 
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
         session, quote_tbl = add_quote_tool_data(
             session,
@@ -461,7 +467,7 @@ def update_table_tool_data(session: Session,
 
         session, tool_md = update_ToolMD(session=session,
                                                  tool_md= tool_md,
-                                                 tool_name= tool_name,
+                                                 tool_name= block.type,
                                                  block_id=block.id)
         session, tt_tbl = add_table_tool_data(
             session,
